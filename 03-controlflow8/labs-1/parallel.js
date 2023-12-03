70cdd7788e2834e33d32aa04d7017cfac7e496f0
fatmacann <canf261012@gmail.com>
2023-12-03 15:37:24 +0300

    controlflow&childprocesses

---
+++ 03-controlflow8/labs-1/parallel.js
@@ -0,0 +1,29 @@
+const print = (err, contents) => {
+  if (err) console.error(err)
+  else console.log(contents )
+}
+
+const opA = (cb) => {
+  setTimeout(() => {
+    cb(null, 'A')
+  }, 500)
+}
+
+const opB = (cb) => {
+  setTimeout(() => {
+    cb(null, 'B')
+  }, 250)
+}
+
+const opC = (cb) => {
+  setTimeout(() => {
+    cb(null, 'C')
+  }, 125)
+}
+
+opA(print)
+opB(print)
+opC(print)
+
+
+//Parallel Execution
